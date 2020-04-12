93a14877e9a25ea92154a7b1c16a1bb633190f20
kbarron525 <35944039+kbarron525@users.noreply.github.com>
2020-04-12 13:59:17 -0400

    Adding quartermaster perks.
    Also ran prettier to print semi-colons at the end of each line.

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,7 @@
-import { writable, derived } from 'svelte/store'
+import { writable, derived } from 'svelte/store';
 
-export const character = writable('Brute')
-export const attackModifiers = createAttackModifiers()
+export const character = writable('Brute');
+export const attackModifiers = createAttackModifiers();
 
 function buildBaseDeck() {
   return [
@@ -25,42 +25,42 @@ function buildBaseDeck() {
     '+2',
     '2x',
     'null',
-  ]
+  ];
 }
 
 function createAttackModifiers() {
-  const attackModifiers = writable(buildBaseDeck())
-  const { subscribe, set, update } = attackModifiers
+  const attackModifiers = writable(buildBaseDeck());
+  const { subscribe, set, update } = attackModifiers;
 
   return {
     subscribe,
     handlePerk: (perk, isChecked) => {
-      const add = card => update(deck => [].concat(deck, card))
+      const add = (card) => update((deck) => [].concat(deck, card));
 
-      const remove = card =>
-        update(deck => {
-          const index = deck.indexOf(card)
+      const remove = (card) =>
+        update((deck) => {
+          const index = deck.indexOf(card);
           if (index === -1) {
-            return deck
+            return deck;
           }
-          return [].concat(deck.slice(0, index), deck.slice(index + 1))
-        })
+          return [].concat(deck.slice(0, index), deck.slice(index + 1));
+        });
 
-      const { modifiersToAdd, modifiersToRemove } = perk
+      const { modifiersToAdd, modifiersToRemove } = perk;
 
       if (isChecked) {
-        modifiersToAdd.forEach(m => add(m))
-        modifiersToRemove.forEach(m => remove(m))
+        modifiersToAdd.forEach((m) => add(m));
+        modifiersToRemove.forEach((m) => remove(m));
       } else {
-        modifiersToAdd.forEach(m => remove(m))
-        modifiersToRemove.forEach(m => add(m))
+        modifiersToAdd.forEach((m) => remove(m));
+        modifiersToRemove.forEach((m) => add(m));
       }
     },
     reset: () => set(buildBaseDeck()),
-  }
+  };
 }
 
-export const availablePerks = derived(character, $character => {
+export const availablePerks = derived(character, ($character) => {
   if ($character === 'Spellweaver') {
     return [
       {
@@ -138,7 +138,61 @@ export const availablePerks = derived(character, $character => {
         modifiersToAdd: ['sun--roll', 'dark--roll'],
         modifiersToRemove: [],
       },
-    ]
+    ];
   }
-  return []
-})
+  if ($character === 'Quartermaster') {
+    return [
+      {
+        text: 'Remove two [-1] cards',
+        modifiersToAdd: [],
+        modifiersToRemove: ['-1', '-1'],
+      },
+      {
+        text: 'Remove four [+0] cards',
+        modifiersToAdd: [],
+        modifiersToRemove: ['+0', '+0', '+0', '+0'],
+      },
+      {
+        text: 'Replace one [+0] card with one [+2] card',
+        modifiersToAdd: ['+2'],
+        modifiersToRemove: ['+0'],
+      },
+      {
+        text: 'Add two [ROLL] [+1] cards',
+        modifiersToAdd: ['+1--roll', '+1--roll'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Add three [ROLL] [MUDDLE] cards',
+        modifiersToAdd: ['muddle--roll', 'muddle--roll', 'muddle--roll'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Add two [ROLL] [PIERCE] card',
+        modifiersToAdd: ['+3--pierce'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Add one [ROLL] [STUN] card',
+        modifiersToAdd: ['+stun--roll'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Add one [ROLL] [TARGET] card',
+        modifiersToAdd: ['+target--roll'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Add one [ROLL] [REFRESH] card',
+        modifiersToAdd: ['refresh--roll'],
+        modifiersToRemove: [],
+      },
+      {
+        text: 'Ignore negative item effects and add two [+1] cards',
+        modifiersToAdd: ['+1', '+1'],
+        modifiersToRemove: [],
+      }
+    ];
+  }
+  return [];
+});
