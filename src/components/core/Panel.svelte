54cabbfb09266095ddeea0b72af5a9b0075bd360
Cody Cahoon <codyjcahoon@gmail.com>
2020-04-13 15:00:12 -0400

    Adds panel component

---
+++ src/components/core/Panel.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let title
+</script>
+
+<style>
+  .panel {
+    background-color: #f2f2f2;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    padding: 20px;
+    margin-right: 20px;
+    border-radius: 2px;
+    flex-grow: 1;
+    width: calc(33% - 20px);
+  }
+
+  .panel__title {
+    color: #222;
+    font-size: 14px;
+    text-transform: uppercase;
+    margin: 0 0 30px 0;
+    font-weight: bold;
+  }
+</style>
+
+<section class="panel">
+  <h1 class="panel__title">{title}</h1>
+  <slot />
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,29 +2,13 @@
   import AttackModifiers from '../components/AttackModifiers.svelte'
   import Characters from '../components/Characters.svelte'
   import Perks from '../components/Perks.svelte'
+
+  import Panel from '../components/core/Panel.svelte'
 </script>
 
 <style>
   main {
     display: flex;
-  }
-
-  .panel {
-    background-color: #f2f2f2;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    padding: 20px;
-    margin-right: 20px;
-    border-radius: 2px;
-    flex-grow: 1;
-    width: calc(33% - 20px);
-  }
-
-  .panel__title {
-    color: #222;
-    font-size: 14px;
-    text-transform: uppercase;
-    margin: 0 0 30px 0;
-    font-weight: bold;
   }
 </style>
 
@@ -33,21 +17,18 @@
 </svelte:head>
 
 <main>
-
   <!-- Character Selection -->
-  <section class="panel">
-    <h2 class="panel__title">Characters</h2>
+  <Panel title="Characters">
     <Characters />
-  </section>
+  </Panel>
 
   <!-- Perk Selection -->
-  <section class="panel">
-    <h2 class="panel__title">Perks</h2>
+  <Panel title="Perks">
     <Perks />
-  </section>
+  </Panel>
 
-  <section class="panel">
-    <h2 class="panel__title">Attack Modifiers</h2>
+  <!-- Attack Modifiers -->
+  <Panel title="Attack Modifiers">
     <AttackModifiers />
-  </section>
+  </Panel>
 </main>
