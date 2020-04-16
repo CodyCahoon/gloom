4e8c357475274effd0217d44046433455adb2847
Cody Cahoon <codyjcahoon@gmail.com>
2020-04-16 14:18:38 -0400

    Begin initial work for showing attack modifiers

--- src/components/AttackModifiers.svelte
+++ src/components/AttackModifiers.svelte
@@ -1,9 +1,28 @@
 <script>
+  import { attackModifiers } from '../stores/index.js';
   import Button from './core/Button.svelte';
 
-  const onDraw = () => {};
+  const { available, drawn } = attackModifiers;
+
+  const onDraw = () => {
+    attackModifiers.draw();
+  };
   const onShuffle = () => {};
 </script>
 
 <Button text="Draw" onClick={onDraw} />
 <Button text="Shuffle" onClick={onShuffle} />
+
+<ul>
+  <label for="">available</label>
+  {#each $available as ac}
+    <li>{ac}</li>
+  {/each}
+</ul>
+
+<ul>
+  <label for="">drawn</label>
+  {#each $drawn as ac}
+    <li>{ac}</li>
+  {/each}
+</ul>
